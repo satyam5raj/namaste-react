9ddbc5c32dc4d0c371e0ccd255afcce9509ef2b4
satyam5raj <satyamraj529@gmail.com>
2024-02-28 13:46:05 +0530

    Used hook to rerender data

--- FoodApp/src/components/Body.js
+++ FoodApp/src/components/Body.js
@@ -1,13 +1,27 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from 'react'
 
 const Body = () => {
+    const [listOfRestaurant, setListOfrestaurant] = useState(resList);
+
+    const handleFilterClick = () => {
+        const filteredList = listOfRestaurant.filter(res => parseFloat(res.info.avgRating) > 4.3);
+        setListOfrestaurant(filteredList)
+        // console.log(filtered); 
+    }
+
     return (
         <div className='body'>
-            <div className='search'>Search</div>
+            <div className='filter'>
+                <button className='filter-btn' 
+                onClick={handleFilterClick}>
+                    Top Rated Restaurants
+                </button>
+            </div>
             <div className='res-container'>
                 {
-                    resList.map((restaurant) => (
+                    listOfRestaurant.map((restaurant) => (
                     <RestaurantCard key={restaurant.info.id} resData={restaurant}/>
                     ))
                 }
